d4bf490b770bc7ceda8f252378fb3292545aeaf7
tiedl25 <max.tiedl@gmx.de>
2024-05-04 17:49:52 +0200

    Add BookCarousel

--- lib/Views/masterview.dart
+++ lib/Views/masterview.dart
@@ -2,6 +2,7 @@ import 'package:bookclub/database.dart';
 import 'package:bookclub/models/book.dart';
 import 'package:bookclub/models/member.dart';
 import 'package:bookclub/models/progress.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 
 class MyHomePage extends StatefulWidget {
@@ -15,7 +16,23 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   late List<Member> members;
+  late List<Book> books;
   late Book book;
+  bool initItems = false;
+  late double aspRat;
+
+  @override
+  void initState() {
+    super.initState();
+
+  }
+
+  init() async {
+    members = await DatabaseHelper.instance.getMemberList();
+    books = await DatabaseHelper.instance.getBookList();
+    book = await DatabaseHelper.instance.getCurrentBook();
+    aspRat = MediaQuery.of(context).size.aspectRatio;
+  }
 
   showUpdateDialog(Progress progress){
     showDialog(context: context, builder: (builder){
@@ -44,66 +61,97 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
+  Widget memberProgress(AsyncSnapshot<List<Progress>> snapshot){
+    return SizedBox(
+      width: MediaQuery.of(context).size.width,
+      height: MediaQuery.of(context).size.height/2,
+      child: ListView.builder(
+        physics: const BouncingScrollPhysics(parent:AlwaysScrollableScrollPhysics()),
+        padding: const EdgeInsets.all(16),
+        itemCount: snapshot.data!.length,
+        itemBuilder: (context, i) {
+          return Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(members.firstWhere((element) => element.id == snapshot.data![i].memberId).name),
+              Row(
+                children: [
+                  IconButton(
+                    onPressed: (){
+                      showUpdateDialog(snapshot.data![i]);
+                    }, 
+                    icon: const Icon(Icons.update),
+                  ),
+                  Container(
+                    width: aspRat > 1 ? MediaQuery.of(context).size.width*0.9 : MediaQuery.of(context).size.width*0.4,
+                    margin: const EdgeInsets.all(20),
+                    child: Column(
+                      children: [
+                        LinearProgressIndicator(
+                          value: snapshot.data![i].page/book.pages,
+                          borderRadius: BorderRadius.circular(5),
+                          color: Color(members.firstWhere((element) => element.id == snapshot.data![i].memberId).color),
+                        ),
+                        Align(
+                          alignment: AlignmentGeometry.lerp(Alignment.topLeft, Alignment.topRight, snapshot.data![i].page/book.pages) as AlignmentGeometry,
+                          child: Text(snapshot.data![i].page == book.pages ? 'Finished' : 'Page ${snapshot.data![i].page} (${(snapshot.data![i].page/book.pages*100).toStringAsFixed(0)}%)'),
+                        )
+                      ]
+                    )
+                  )
+                ],
+              )
+              
+            ],
+          );
+        }
+      ),
+    );
+  }
+  
+  Widget bookCarousel(){
+    return CarouselSlider(
+      options: CarouselOptions(
+        height: MediaQuery.of(context).size.height/2,
+        viewportFraction: 0.35/aspRat,//aspRat < 1 ? 0.7 : 0.2,
+        initialPage: book.id!-1,
+        enableInfiniteScroll: false,
+        reverse: false,
+        autoPlay: false,
+        enlargeCenterPage: true,
+        enlargeFactor: 0.3,
+        scrollDirection: Axis.horizontal,
+        onPageChanged: (index, reason) => setState(() => book = books[index]),
+      ),
+      items: books.map((i) {
+        return ClipRRect(
+              borderRadius: BorderRadius.circular(20),
+              child: Image.network(i.image_path),
+            );
+          
+      }).toList(),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
+    aspRat = MediaQuery.of(context).size.aspectRatio;
     return Scaffold(
       body: Center(
         child: FutureBuilder(
-              future: getMembers(),
-              builder: (BuildContext context, AsyncSnapshot<List<Progress>> snapshot) {
-                if (!snapshot.hasData){
-                  return const Center(child: CircularProgressIndicator());
-                }
-                return Column(
-                  children: [
-                    SizedBox(
-                      height: MediaQuery.of(context).size.height/2,
-                      child: Image.network(book.image_path),
-                    ),
-                    SizedBox(
-                      width: MediaQuery.of(context).size.width,
-                      height: MediaQuery.of(context).size.height/2,
-                      child: ListView.builder(
-                      physics: const BouncingScrollPhysics(parent:AlwaysScrollableScrollPhysics()),
-                      padding: const EdgeInsets.all(16),
-                      itemCount: snapshot.data!.length,
-                      itemBuilder: (context, i) {
-                        return Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text(members.firstWhere((element) => element.id == snapshot.data![i].memberId).name),
-                            IconButton(
-                              onPressed: (){
-                                showUpdateDialog(snapshot.data![i]);
-                              }, 
-                              icon: const Icon(Icons.update),
-                            ),
-                            Container(
-                              width: MediaQuery.of(context).size.width*0.9,
-                              margin: const EdgeInsets.all(20),
-                              child: Column(
-                                children: [
-                                  LinearProgressIndicator(
-                                    value: snapshot.data![i].page/book.pages,
-                                    borderRadius: BorderRadius.circular(5),
-                                    color: Color(members.firstWhere((element) => element.id == snapshot.data![i].memberId).color),
-                                  ),
-                                  Align(
-                                    alignment: AlignmentGeometry.lerp(Alignment.topLeft, Alignment.topRight, snapshot.data![i].page/book.pages) as AlignmentGeometry,
-                                    child: Text(snapshot.data![i].page == book.pages ? 'Finished' : 'Page ${snapshot.data![i].page} (${(snapshot.data![i].page/book.pages*100).toStringAsFixed(0)}%)'),
-                                  )
-                                ]
-                              )
-                            )
-                          ],
-                        );
-                      }
-                    ),
-                    )
-                  ]
-                );
-              },
-            )    
+          future: getProgress(),
+          builder: (BuildContext context, AsyncSnapshot<List<Progress>> snapshot) {
+            if (!snapshot.hasData){
+              return const Center(child: CircularProgressIndicator());
+            }
+            return Column(
+              children: [
+                bookCarousel(),
+                memberProgress(snapshot)
+              ]
+            );
+          },
+      )    
     )
     );
   }
@@ -112,11 +160,11 @@ class _MyHomePageState extends State<MyHomePage> {
     DatabaseHelper.instance.updateProgress(progress);
   }
 
-  Future<List<Progress>> getMembers() async {
-    members = await DatabaseHelper.instance.getMemberList();
-    
-    book = await DatabaseHelper.instance.getCurrentBook();
-    
+  Future<List<Progress>> getProgress() async {
+    if (!initItems){
+      await init();
+      initItems = true;
+    } 
     List<Progress> progress = await DatabaseHelper.instance.getProgressList(book.id!);
     return progress;
   }
