ecdb0b62b739275c1d438dff09827f5e4c74e83c
tiedl25 <max.tiedl@gmx.de>
2024-05-10 22:48:11 +0200

    Add Comment model

---
+++ lib/models/comment.dart
@@ -0,0 +1,24 @@
+class Comment{
+  int? id;
+  String text;
+  int? bookId;
+  int? memberId;
+
+  Comment({this.id, required this.text, this.bookId, this.memberId});
+
+  Map<String, dynamic> toMap() => {
+    'id': id,
+    'text': text,
+    'book': bookId,
+    'member': memberId
+  };
+
+  factory Comment.fromMap(Map<String, dynamic> map) {
+    return Comment(
+      id: map['id'],
+      text: map['text'],
+      bookId: map['book'],
+      memberId: map['member']
+    );
+  }
+}
