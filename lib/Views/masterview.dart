8ce58c39dcbdb28d615bcda5b578c516b460a76e
tiedl25 <max.tiedl@gmx.de>
2024-05-18 14:59:03 +0200

    Refactor Masterview

--- lib/Views/masterview.dart
+++ lib/Views/masterview.dart
@@ -29,12 +29,6 @@ class _MyHomePageState extends State<MyHomePage> {
   late double nameMaxLength;
   int selectedMember = 1;
 
-  @override
-  void initState() {
-    super.initState();
-
-  }
-
   Future<void> init() async {
     members = await DatabaseHelper.instance.getMemberList();
     books = await DatabaseHelper.instance.getBookList();
@@ -42,6 +36,86 @@ class _MyHomePageState extends State<MyHomePage> {
     aspRat = MediaQuery.of(context).size.aspectRatio;
     nameMaxLength = members.map((e) => e.name.length).toList().reduce(max)*10;
   }
+
+  int daysLeft(){
+    return book.to.difference(DateTime.now()).inDays;
+  }
+
+  int minimumPages(){
+    return (book.pages/book.from.difference(book.to).inDays*book.from.difference(DateTime.now()).inDays).toInt();
+  }
+
+  Future<void> updatePage(Progress progress) async {
+    DatabaseHelper.instance.updateProgress(progress);
+  }
+
+  Future<List<Progress>> getProgress() async {
+    if (!initItems){
+      await init();
+      initItems = true;
+    } 
+    comments = await DatabaseHelper.instance.getComments(book.id!);
+    List<Progress> progress = await DatabaseHelper.instance.getProgressList(book.id!);
+    return progress;
+  }
+
+  void addComment(String value, [void Function(VoidCallback fn)? setState]){
+    if(value == ''){
+      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text('Please enter a comment.')));
+      return;
+    }
+    this.setState(() {
+      DatabaseHelper.instance.addComment(Comment(text: value, bookId: book.id!, memberId: selectedMember));
+    });
+    
+    if (setState != null) setState(() {comments.add(Comment(text: value, bookId: book.id!, memberId: selectedMember));});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    aspRat = MediaQuery.of(context).size.aspectRatio;
+    return Scaffold(
+      body: Center(
+        child: FutureBuilder(
+          future: getProgress(),
+          builder: (BuildContext context, AsyncSnapshot<List<Progress>> snapshot) {
+            if (!snapshot.hasData){
+              return const Center(child: CircularProgressIndicator());
+            }
+            return Column(
+              children: [
+                const Spacer(flex: 1),
+                bookCarousel(),
+                const Spacer(flex: 1),
+                AutoSizeText('${book.name} von ${book.author} - ${book.pages} Seiten', textAlign: TextAlign.center, minFontSize: 18,),
+                if (book.to.difference(DateTime.now()).inDays > 0) 
+                  AutoSizeText('Du hast noch ${daysLeft()} Tage um das Buch zu lesen. Die Zeit rennt!!!', textAlign: TextAlign.center, minFontSize: 18,),
+                if (book.to.difference(DateTime.now()).inDays > 0) 
+                  AutoSizeText('Seite ${minimumPages()} sollte jetzt schon drin sein.', textAlign: TextAlign.center, minFontSize: 18,),
+                const Divider(),
+                Expanded(flex: 20, child: 
+                  Row(
+                    children: [
+                      Expanded(flex: 20, child: memberBoard(snapshot)),
+                      if (aspRat > 1) Expanded(flex: 10, child: commentBoard()),
+                    ]
+                  ),
+                ),
+                if (aspRat < 1) IconButton(
+                  onPressed: (){
+                    showCommentDialog();
+                  },
+                  icon: const Icon(Icons.comment)
+                )
+              ]
+            );
+          },
+        )    
+      )
+    );
+  }
+
+  //Dialogs
 
   void showUpdateDialog(Progress progress){
     showDialog(context: context, builder: (builder){
@@ -89,47 +163,64 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
-  Widget rating(Progress progress) {
-    return Row(
-      children: List.generate(
-        7, 
-        (index) {
-          return InkWell(
-            child: Icon(Icons.book, color: progress.rating == null || progress.rating! < index + 1 ? bookDefaultColor : bookSelectedColor),
-            onTap: () {
-              if (progress.rating == index+1){
-                progress.rating = 0;
-              } else {
-                progress.rating = index + 1;
-              }
-              
-              setState(() {
-                DatabaseHelper.instance.updateProgress(progress);
-              });
+  void showDeleteDialog(Comment comment, [void Function(VoidCallback fn)? setState]) {
+    showDialog(context: context, builder: (builder){
+      return AlertDialog(
+        title: const Text("Delete comment"),
+        content: const Text("Are you sure you want to delete this comment?"),
+        actions: [
+          TextButton(
+            onPressed: () => Navigator.pop(context),
+            child: const Text("Cancel"),
+          ),
+          TextButton(
+            onPressed: (){
+              this.setState(() { DatabaseHelper.instance.deleteComment(comment.id!); });
+              if (setState != null) setState(() {comments.remove(comment);});
+              Navigator.pop(context);
             },
-          );
-        }
-        )
+            child: const Text("Delete"),
+          )
+        ]
+      );
+    });
+  }
+
+  //Widgets
+
+  Widget bookCarousel(){
+    return CarouselSlider(
+      options: CarouselOptions(
+        height: MediaQuery.of(context).size.height/(aspRat<1 ? 3 : 2),
+        viewportFraction: aspRat < 1 ? 0.25/aspRat : 0.4/aspRat,
+        initialPage: book.id!-1,
+        enableInfiniteScroll: false,
+        reverse: false,
+        autoPlay: false,
+        enlargeCenterPage: true,
+        enlargeFactor: 0.25,
+        scrollDirection: Axis.horizontal,
+        onPageChanged: (index, reason) => setState(() => book = books[index]),
+      ),
+      items: books.map((i) {
+        return ClipRRect(
+          borderRadius: BorderRadius.circular(20),
+          child: Image.network(i.image_path),
+        );
+      }).toList(),
     );
   }
 
-  Widget progressIndicator(Progress progress){
-    return Stack(
-      children: [
-        LinearProgressIndicator(
-          minHeight: 20,
-          value: progress.page/book.pages,
-          borderRadius: BorderRadius.circular(10),
-          color: Color(members.firstWhere((element) => element.id == progress.memberId).color),
-        ),
-        Align(
-          alignment: AlignmentGeometry.lerp(Alignment.bottomLeft, Alignment.bottomRight, progress.page/book.pages) as AlignmentGeometry,
-          child: Container(
-            padding: const EdgeInsets.symmetric(horizontal: 5),
-            child: Text(progress.page == book.pages ? 'Finished' : 'Seite ${progress.page} (${(progress.page/book.pages*100).toStringAsFixed(0)}%)')
-          ),
-        )
-      ]
+  Widget memberBoard(AsyncSnapshot<List<Progress>> snapshot){
+    return SizedBox(
+      child: ListView.builder(
+        physics: const BouncingScrollPhysics(parent:AlwaysScrollableScrollPhysics()),
+        padding: const EdgeInsets.only(left: 10, right: 10),
+        itemCount: snapshot.data!.length,
+        itemBuilder: (context, i) {
+          return aspRat < 1 ? mobileProgress(snapshot.data![i]) : desktopProgress(snapshot.data![i]);
+        }
+      ),
     );
   }
 
@@ -139,7 +230,7 @@ class _MyHomePageState extends State<MyHomePage> {
         Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            Container(
+            SizedBox(
               width: nameMaxLength,
               child: Text(members.firstWhere((element) => element.id == progress.memberId).name)
             ),
@@ -152,7 +243,7 @@ class _MyHomePageState extends State<MyHomePage> {
             Expanded(child: rating(progress)),
           ],
         ),
-        Container(
+        SizedBox(
           width: MediaQuery.of(context).size.width*0.9,
           child: progressIndicator(progress),
         )
@@ -185,53 +276,89 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 
-    Widget bookCarousel(){
-    return CarouselSlider(
-      options: CarouselOptions(
-        height: MediaQuery.of(context).size.height/(aspRat<1 ? 3 : 2),
-        viewportFraction: aspRat < 1 ? 0.25/aspRat : 0.4/aspRat,
-        initialPage: book.id!-1,
-        enableInfiniteScroll: false,
-        reverse: false,
-        autoPlay: false,
-        enlargeCenterPage: true,
-        enlargeFactor: 0.25,
-        scrollDirection: Axis.horizontal,
-        onPageChanged: (index, reason) => setState(() => book = books[index]),
-      ),
-      items: books.map((i) {
-        return ClipRRect(
-              borderRadius: BorderRadius.circular(20),
-              child: Image.network(i.image_path),
-            );
-          
-      }).toList(),
+  Widget progressIndicator(Progress progress){
+    return Stack(
+      children: [
+        LinearProgressIndicator(
+          minHeight: 20,
+          value: progress.page/book.pages,
+          borderRadius: BorderRadius.circular(10),
+          color: Color(members.firstWhere((element) => element.id == progress.memberId).color),
+        ),
+        Align(
+          alignment: AlignmentGeometry.lerp(Alignment.bottomLeft, Alignment.bottomRight, progress.page/book.pages) as AlignmentGeometry,
+          child: Container(
+            padding: const EdgeInsets.symmetric(horizontal: 5),
+            child: Text(progress.page == book.pages ? 'Finished' : 'Seite ${progress.page} (${(progress.page/book.pages*100).toStringAsFixed(0)}%)')
+          ),
+        )
+      ]
     );
   }
 
-  Widget memberProgress(AsyncSnapshot<List<Progress>> snapshot){
-    return SizedBox(
-      child: ListView.builder(
-        physics: const BouncingScrollPhysics(parent:AlwaysScrollableScrollPhysics()),
-        padding: const EdgeInsets.only(left: 10, right: 10),
-        itemCount: snapshot.data!.length,
-        itemBuilder: (context, i) {
-          return aspRat < 1 ? mobileProgress(snapshot.data![i]) : desktopProgress(snapshot.data![i]);
+  Widget rating(Progress progress) {
+    return Row(
+      children: List.generate(
+        7, 
+        (index) {
+          return InkWell(
+            child: Icon(Icons.book, color: progress.rating == null || progress.rating! < index + 1 ? bookDefaultColor : bookSelectedColor),
+            onTap: () {
+              if (progress.rating == index+1){
+                progress.rating = 0;
+              } else {
+                progress.rating = index + 1;
+              }
+              setState(() {
+                DatabaseHelper.instance.updateProgress(progress);
+              });
+            },
+          );
         }
-      ),
+      )
     );
   }
 
-  void addComment(String value, [void Function(VoidCallback fn)? setState]){
-    if(value == ''){
-      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text('Please enter a comment.')));
-      return;
-    }
-    this.setState(() {
-      DatabaseHelper.instance.addComment(Comment(text: value, bookId: book.id!, memberId: selectedMember));
-    });
-    
-    if (setState != null) setState(() {comments.add(Comment(text: value, bookId: book.id!, memberId: selectedMember));});
+  Widget commentBoard([void Function(VoidCallback fn)? setState]){
+    return Column(
+      children: [
+        Expanded(
+          child: ListView.builder(
+            padding: const EdgeInsets.only(top: 10),
+            itemCount: comments.length,
+            itemBuilder: (BuildContext context, int i) {
+              return GestureDetector(
+                onLongPress: () {
+                  showDeleteDialog(comments[i], setState);
+                },
+                child: Align(
+                  alignment: Alignment.topLeft,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(15),
+                      color: Color(members.firstWhere((element) => element.id == comments[i].memberId).color),
+                    ),
+                    padding: const EdgeInsets.all(5),
+                    margin: const EdgeInsets.only(left: 10, right: 10, bottom: 10),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(comments[i].text, style: const TextStyle(fontSize: 15)),
+                        Text(members.firstWhere((element) => element.id == comments[i].memberId).name, style: const TextStyle(fontSize: 10)),
+                      ],
+                    )
+                  ),
+                )
+              );
+            },
+          ),
+        ),
+        Align(
+            alignment: Alignment.bottomLeft,
+            child: commentField(setState)
+        ),
+      ]
+    );
   }
 
   Widget commentField([void Function(VoidCallback fn)? setState]){
@@ -289,136 +416,5 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
       )
     );
-  }
-
-  Widget commentBoard([void Function(VoidCallback fn)? setState]){
-    return Column(
-      children: [
-        Expanded(
-          child: ListView.builder(
-            padding: const EdgeInsets.only(top: 10),
-            itemCount: comments.length,
-            itemBuilder: (BuildContext context, int i) {
-              return GestureDetector(
-                onLongPress: () {
-                  showDeleteDialog(comments[i], setState);
-                },
-                child: Align(
-                  alignment: Alignment.topLeft,
-                  child: Container(
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(15),
-                      color: Color(members.firstWhere((element) => element.id == comments[i].memberId).color),
-                    ),
-                    padding: const EdgeInsets.all(5),
-                    margin: const EdgeInsets.only(left: 10, right: 10, bottom: 10),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(comments[i].text, style: const TextStyle(fontSize: 15)),
-                        Text(members.firstWhere((element) => element.id == comments[i].memberId).name, style: const TextStyle(fontSize: 10)),
-                      ],
-                    )
-                  ),
-                )
-              );
-            },
-          ),
-        ),
-        Align(
-            alignment: Alignment.bottomLeft,
-            child: commentField(setState)
-          ),
-      ]
-    );
-  }
-
-  int daysLeft(){
-    return book.to.difference(DateTime.now()).inDays;
-  }
-
-  int minimumPages(){
-    return (book.pages/book.from.difference(book.to).inDays*book.from.difference(DateTime.now()).inDays).toInt();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    aspRat = MediaQuery.of(context).size.aspectRatio;
-    return Scaffold(
-      body: Center(
-        child: FutureBuilder(
-          future: getProgress(),
-          builder: (BuildContext context, AsyncSnapshot<List<Progress>> snapshot) {
-            if (!snapshot.hasData){
-              return const Center(child: CircularProgressIndicator());
-            }
-            return Column(
-              children: [
-                const Spacer(flex: 1),
-                bookCarousel(),
-                const Spacer(flex: 1),
-                AutoSizeText('${book.name} von ${book.author} - ${book.pages} Seiten', textAlign: TextAlign.center, minFontSize: 18,),
-                if (book.to.difference(DateTime.now()).inDays > 0) 
-                  AutoSizeText('Du hast noch ${daysLeft()} Tage um das Buch zu lesen. Die Zeit rennt!!!', textAlign: TextAlign.center, minFontSize: 18,),
-                if (book.to.difference(DateTime.now()).inDays > 0) 
-                  AutoSizeText('Seite ${minimumPages()} sollte jetzt schon drin sein.', textAlign: TextAlign.center, minFontSize: 18,),
-                const Divider(),
-                Expanded(flex: 20, child: 
-                  Row(
-                    children: [
-                      Expanded(flex: 20, child: memberProgress(snapshot)),
-                      if (aspRat > 1) Expanded(flex: 10, child: commentBoard()),
-                    ]
-                  ),
-                ),
-                if (aspRat < 1) IconButton(
-                  onPressed: (){
-                    showCommentDialog();
-                  },
-                  icon: const Icon(Icons.comment)
-                )
-              ]
-            );
-          },
-        )    
-      )
-    );
-  }
-
-  Future<void> updatePage(Progress progress) async {
-    DatabaseHelper.instance.updateProgress(progress);
-  }
-
-  Future<List<Progress>> getProgress() async {
-    if (!initItems){
-      await init();
-      initItems = true;
-    } 
-    comments = await DatabaseHelper.instance.getComments(book.id!);
-    List<Progress> progress = await DatabaseHelper.instance.getProgressList(book.id!);
-    return progress;
-  }
-  
-  void showDeleteDialog(Comment comment, [void Function(VoidCallback fn)? setState]) {
-    showDialog(context: context, builder: (builder){
-      return AlertDialog(
-        title: const Text("Delete comment"),
-        content: const Text("Are you sure you want to delete this comment?"),
-        actions: [
-          TextButton(
-            onPressed: () => Navigator.pop(context),
-            child: const Text("Cancel"),
-          ),
-          TextButton(
-            onPressed: (){
-              this.setState(() { DatabaseHelper.instance.deleteComment(comment.id!); });
-              if (setState != null) setState(() {comments.remove(comment);});
-              Navigator.pop(context);
-            },
-            child: const Text("Delete"),
-          )
-        ]
-      );
-    });
   }
 }
