05fe5752889686700660a700cae92f7237abce31
tiedl25 <max.tiedl@gmx.de>
2024-12-29 14:58:01 +0100

    Change Theme seed color

--- lib/colors.dart
+++ lib/colors.dart
@@ -3,8 +3,8 @@ import 'package:flutter/material.dart';
 Color bookSelectedColor = Colors.brown.shade400;
 Color bookDefaultColor = Colors.grey.shade400;
 
-Color backgroundColor = Color.fromARGB(255, 240, 237, 221);
-Color themeColor = Color.fromARGB(255, 212, 210, 185);
+Color backgroundColor = Color.fromARGB(255, 242, 240, 227);
+Color themeColor = Color.fromARGB(255, 18, 65, 135);
 
 ColorScheme colorScheme = ColorScheme.fromSeed(
   seedColor: themeColor,
