022c710f823f513c45b25d74c804bc54f9908de4
tiedl25 <max.tiedl@gmx.de>
2025-01-05 16:00:30 +0100

    Add MostLikedBook Statistic

--- lib/dialogs/statisticsDialog.dart
+++ lib/dialogs/statisticsDialog.dart
@@ -22,6 +22,13 @@ class _StatisticsDialogState extends State<StatisticsDialog> {
   late Future<List<Progress>> progressFuture;
   late List<Progress> progress;
 
+  final icons = [
+  const Icon(Icons.star, color: SpecialColors.gold, size: 40),
+    Container(padding: const EdgeInsets.symmetric(horizontal: 5), child: const Icon(Icons.star, color: SpecialColors.silver, size: 30)),
+    Container(padding: const EdgeInsets.symmetric(horizontal: 7.5), child: const Icon(Icons.star, color: SpecialColors.bronze, size: 25)),
+    Padding(padding: const EdgeInsets.symmetric(horizontal: 10), child: Icon(Icons.book, color: SpecialColors.bookSelectedColor, size: 20))
+  ];
+
   @override
   void initState() {
     super.initState();
@@ -35,8 +42,7 @@ class _StatisticsDialogState extends State<StatisticsDialog> {
       if (memberProgress.isNotEmpty){
         final parts = memberProgress.map((e) => e.page / (e.maxPages ?? widget.books[e.bookId!-1].pages)).toList();
         double overalProgress = parts.reduce((element, value) => element + value) / parts.length;
-        //final test = memberProgress.reduce((element, value) => Progress(page: element.page + value.page, maxPages: (element.maxPages ?? widget.books[element.bookId!-1].pages) + (value.maxPages ?? widget.books[value.bookId!-1].pages)));
-        progressByMember[member.name] = overalProgress;//test.page / test.maxPages!;
+        progressByMember[member.name] = overalProgress;
       }
     }
     return Map.fromEntries(progressByMember.entries.toList()..sort((e1, e2) => e1.value.compareTo(e2.value)));
@@ -44,13 +50,6 @@ class _StatisticsDialogState extends State<StatisticsDialog> {
 
   Widget wallOfShame() {
     final progressByMember = nominateShamePerson().entries.toList();
-
-    final icons = [
-      const Icon(Icons.star, color: SpecialColors.gold, size: 40),
-      Container(padding: const EdgeInsets.symmetric(horizontal: 5), child: const Icon(Icons.star, color: SpecialColors.silver, size: 30)),
-      Container(padding: const EdgeInsets.symmetric(horizontal: 7.5), child: const Icon(Icons.star, color: SpecialColors.bronze, size: 25)),
-      Padding(padding: const EdgeInsets.symmetric(horizontal: 10), child: Icon(Icons.book, color: SpecialColors.bookSelectedColor, size: 20))
-    ];
 
     int iconIndex=0;
     final memberIcons = [];
@@ -96,6 +95,68 @@ class _StatisticsDialogState extends State<StatisticsDialog> {
     );
   }
 
+  Map<String, double> bookRatingList(){
+    Map<String, double> ratingByBook = {};
+    for (var book in widget.books){
+      var bookProgress = progress.where((element) => element.bookId == book.id && element.rating != null && element.page > 0);
+      if (bookProgress.isNotEmpty){
+        final parts = bookProgress.map((e) => e.rating ?? 0).toList();
+        double overalRating = parts.reduce((element, value) => element + value) / parts.length;
+        ratingByBook[book.name] = overalRating;
+      }
+    }
+    return Map.fromEntries(ratingByBook.entries.toList()..sort((e1, e2) => e2.value.compareTo(e1.value)));
+  }
+
+  Widget bestRankedBook(){
+    final ratingByBook = bookRatingList().entries.toList();
+
+    int iconIndex=0;
+    final bookIcons = [];
+
+    for (var i = 0; i < ratingByBook.length; i++){
+      if (i == 0) bookIcons.add(icons[0]);
+      else if (ratingByBook[i].value == ratingByBook[i-1].value || iconIndex > 2){
+        bookIcons.add(icons[iconIndex]);
+      } else {
+        bookIcons.add(icons[iconIndex+1]);
+        if (iconIndex<3) iconIndex++;
+      }
+    }
+
+    return Column(
+      children: [
+        const Text(CustomStrings.statisticsTitle2, style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+        ListView.builder(
+          shrinkWrap: true,
+          itemCount: ratingByBook.length,
+          itemBuilder: (context, index) {
+            final e = ratingByBook[index];
+
+            return Row(
+              mainAxisAlignment: MainAxisAlignment.start, 
+              children: [
+                bookIcons[index],
+                const SizedBox(width: 5),
+                Expanded(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Flexible(flex: 10, child: Text(e.key, style: const TextStyle(fontSize: 16))),
+                      const Spacer(),
+                      Text("${(e.value).toStringAsFixed(2)} ", style: const TextStyle(fontSize: 16)),
+                    ]
+                  )
+                ),
+                Icon(Icons.book, color: SpecialColors.bookSelectedColor, size: 16)
+              ]
+            );
+          },          
+        )
+      ]
+    );
+  }
+
   Widget phone(){
     return CustomDialog(
       fullWindow: true,
@@ -104,6 +165,8 @@ class _StatisticsDialogState extends State<StatisticsDialog> {
         child: Column(
           children: [
             wallOfShame(),
+            const SizedBox(height: 30),
+            bestRankedBook()
           ]
         )
       ),
@@ -116,6 +179,8 @@ class _StatisticsDialogState extends State<StatisticsDialog> {
       child: Column(
         children: [
           wallOfShame(),
+          const SizedBox(height: 50),
+          bestRankedBook()
         ]
       )
     );
--- lib/resources/strings.dart
+++ lib/resources/strings.dart
@@ -2,6 +2,7 @@ class CustomStrings {
   static const String appName = 'Book Club';
   static const String appDesription = 'A simple website to manage a bookclub';
   static const String statisticsTitle = "Wall of Shame";
+  static const String statisticsTitle2 = "Most liked books";
   static const String deleteCommentDialogTitle = "Delete comment";
   static const String deleteCommentDialogContent = "Are you sure you want to delete this comment?";
   static const String finishDialogTitle = "Breaking news";
